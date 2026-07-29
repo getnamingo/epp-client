50388d5a8b6bff33c29f8d7b514142d00f79ad8e
Namingo <help@namingo.org>
2026-07-29 12:23:51 +0300

    Update GrEpp.php

--- src/Registries/GrEpp.php
+++ src/Registries/GrEpp.php
@@ -166,6 +166,106 @@ class GrEpp extends Epp
     }
 
     /**
+     * contactCreate
+     */
+    public function contactCreate($params = array())
+    {
+        if (!$this->isLoggedIn) {
+            return array(
+                'code' => 2002,
+                'msg' => 'Command use error'
+            );
+        }
+
+        $return = array();
+        try {
+            $from = $to = array();
+            $from[] = '/{{ type }}/';
+            $to[] = htmlspecialchars($params['type']);
+            $from[] = '/{{ id }}/';
+            $to[] = htmlspecialchars($params['id']);
+            $from[] = '/{{ name }}/';
+            $to[] = htmlspecialchars($params['firstname'] . ' ' . $params['lastname']);
+            $from[] = '/{{ org }}/';
+            $to[] = htmlspecialchars($params['companyname']);
+            $from[] = '/{{ street1 }}/';
+            $to[] = htmlspecialchars($params['address1']);
+            $from[] = '/{{ street2 }}/';
+            $to[] = htmlspecialchars($params['address2']);
+            $from[] = '/{{ street3 }}/';
+            $street3 = (isset($params['address3']) ? $params['address3'] : '');
+            $to[] = htmlspecialchars($street3);
+            $from[] = '/{{ city }}/';
+            $to[] = htmlspecialchars($params['city']);
+            $from[] = '/{{ state }}/';
+            $to[] = htmlspecialchars($params['state']);
+            $from[] = '/{{ postcode }}/';
+            $to[] = htmlspecialchars($params['postcode']);
+            $from[] = '/{{ country }}/';
+            $to[] = htmlspecialchars($params['country']);
+            $from[] = '/{{ phonenumber }}/';
+            $to[] = htmlspecialchars($params['fullphonenumber']);
+            $from[] = '/{{ email }}/';
+            $to[] = htmlspecialchars($params['email']);
+            $from[] = '/{{ clTRID }}/';
+            $microtime = str_replace('.', '', round(microtime(1), 3));
+            $to[] = htmlspecialchars($this->prefix . '-contact-create-' . $microtime);
+            $from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+            $to[] = '';
+            $xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+  <command>
+    <create>
+      <contact:create
+       xmlns:contact="urn:ietf:params:xml:ns:contact-1.0">
+        <contact:id>{{ id }}</contact:id>
+        <contact:postalInfo type="{{ type }}">
+          <contact:name>{{ name }}</contact:name>
+          <contact:org>{{ org }}</contact:org>
+          <contact:addr>
+            <contact:street>{{ street1 }}</contact:street>
+            <contact:street>{{ street2 }}</contact:street>
+            <contact:street>{{ street3 }}</contact:street>
+            <contact:city>{{ city }}</contact:city>
+            <contact:sp>{{ state }}</contact:sp>
+            <contact:pc>{{ postcode }}</contact:pc>
+            <contact:cc>{{ country }}</contact:cc>
+          </contact:addr>
+        </contact:postalInfo>
+        <contact:voice>{{ phonenumber }}</contact:voice>
+        <contact:fax></contact:fax>
+        <contact:email>{{ email }}</contact:email>
+        <contact:authInfo>
+          <contact:pw />
+        </contact:authInfo>
+      </contact:create>
+    </create>
+    <clTRID>{{ clTRID }}</clTRID>
+  </command>
+</epp>');
+            $r = $this->writeRequest($xml);
+            $code = (int)$r->response->result->attributes()->code;
+            $msg = (string)$r->response->result->msg;
+            $r = $r->response->resData->children('urn:ietf:params:xml:ns:contact-1.0')->creData;
+            $id = (string)$r->id;
+
+            $return = array(
+                'code' => $code,
+                'msg' => $msg,
+                'id' => $id
+            );
+        } catch (\Exception $e) {
+            $return = array(
+                'error' => $e->getMessage()
+            );
+        }
+
+        return $return;
+    }
+
+    /**
      * domainCheckClaims
      */
     public function domainCheckClaims($params = array())
@@ -462,7 +562,114 @@ class GrEpp extends Epp
 
         throw new EppException("Launch extension not supported!");
     }
-    
+
+    /**
+     * domainCreate
+     */
+    public function domainCreate($params = array())
+    {
+        if (!$this->isLoggedIn) {
+            return array(
+                'code' => 2002,
+                'msg' => 'Command use error'
+            );
+        }
+
+        $return = array();
+        try {
+            $from = $to = array();
+            $from[] = '/{{ name }}/';
+            $to[] = htmlspecialchars($params['domainname']);
+            $from[] = '/{{ period }}/';
+            $to[] = (int)($params['period']);
+
+            $text = '';
+            if (!empty($params['nss']) && is_array($params['nss'])) {
+                foreach ($params['nss'] as $hostObj) {
+                    if (!is_array($hostObj)) {
+                        $text .= '<domain:hostObj>' . $hostObj . '</domain:hostObj>' . "\n";
+                        continue;
+                    }
+
+                    $text .= "<domain:hostAttr>\n";
+                    if (!empty($hostObj['hostName'])) {
+                        $text .= '  <domain:hostName>' . htmlspecialchars($hostObj['hostName']) . "</domain:hostName>\n";
+                    }
+                    if (!empty($hostObj['ipv4'])) {
+                        $text .= '  <domain:hostAddr ip="v4">' . htmlspecialchars($hostObj['ipv4']) . "</domain:hostAddr>\n";
+                    }
+                    if (!empty($hostObj['ipv6'])) {
+                        $text .= '  <domain:hostAddr ip="v6">' . htmlspecialchars($hostObj['ipv6']) . "</domain:hostAddr>\n";
+                    }
+                    $text .= "</domain:hostAttr>\n";
+                }
+            }
+            $from[] = '/{{ hostObjs }}/';
+            $to[]   = $text;
+
+            $from[] = '/{{ registrant }}/';
+            $to[]   = isset($params['registrant']) ? htmlspecialchars($params['registrant']): '';
+            $text = '';
+            if (!empty($params['contacts']) && is_array($params['contacts'])) {
+                foreach ($params['contacts'] as $contactType => $contactID) {
+                    $text .= '<domain:contact type="' . $contactType . '">' . $contactID . '</domain:contact>' . "\n";
+                }
+            }
+            $from[] = '/{{ contacts }}/';
+            $to[] = $text;
+
+            $from[] = '/{{ clTRID }}/';
+            $clTRID = str_replace('.', '', round(microtime(1), 3));
+            $to[] = htmlspecialchars($this->prefix . '-domain-create-' . $clTRID);
+            $from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+            $to[] = '';
+            $xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+  <command>
+    <create>
+      <domain:create
+       xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
+        <domain:name>{{ name }}</domain:name>
+        <domain:period unit="y">{{ period }}</domain:period>
+        <domain:ns>
+          {{ hostObjs }}
+        </domain:ns>
+        <domain:registrant>{{ registrant }}</domain:registrant>
+        {{ contacts }}
+        <domain:authInfo>
+          <domain:pw />
+        </domain:authInfo>
+      </domain:create>
+    </create>
+    <clTRID>{{ clTRID }}</clTRID>
+  </command>
+</epp>');
+            $r = $this->writeRequest($xml);
+            $code = (int)$r->response->result->attributes()->code;
+            $msg = (string)$r->response->result->msg;
+            $r = $r->response->resData->children('urn:ietf:params:xml:ns:domain-1.0')->creData;
+            $name = (string)$r->name;
+            $crDate = (string)$r->crDate;
+            $exDate = (string)$r->exDate;
+
+            $return = array(
+                'code' => $code,
+                'msg' => $msg,
+                'name' => $name,
+                'crDate' => $crDate,
+                'exDate' => $exDate
+            );
+        } catch (\Exception $e) {
+            $return = array(
+                'error' => $e->getMessage()
+            );
+        }
+
+        return $return;
+    }
+
     /**
      * domainCreateDNSSEC
      */
@@ -524,8 +731,6 @@ class GrEpp extends Epp
             <secDNS:digest>".htmlspecialchars($params['digest_2'])."</secDNS:digest>
           </secDNS:dsData>";
             }
-            $from[] = '/{{ authInfoPw }}/';
-            $to[] = htmlspecialchars($params['authInfoPw']);
             $from[] = '/{{ clTRID }}/';
             $clTRID = str_replace('.', '', round(microtime(1), 3));
             $to[] = htmlspecialchars($this->prefix . '-domain-createDNSSEC-' . $clTRID);
@@ -547,7 +752,7 @@ class GrEpp extends Epp
         <domain:registrant>{{ registrant }}</domain:registrant>
         {{ contacts }}
         <domain:authInfo>
-          <domain:pw>{{ authInfoPw }}</domain:pw>
+          <domain:pw />
         </domain:authInfo>
       </domain:create>
     </create>
